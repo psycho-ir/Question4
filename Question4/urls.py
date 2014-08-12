c842187b0aed89527d54df9aaa4ac1a3e981ffa8
soroosh <soroosh.sarabadani@gmail.com>
2014-08-12 14:10:22 +0430

    some changes

--- Question4/urls.py
+++ Question4/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from ws_client.views import ShowIPView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,5 +10,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'Question4.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'', ShowIPView.as_view()),
 )
--- ws_client/views.py
+++ ws_client/views.py
@@ -6,6 +6,20 @@ from ws_client.client import resolve_country_from_ip
 
 class ShowIPView(View):
     def get(self, request):
-        print request
-        result = resolve_country_from_ip('4.2.2.4')
-        render_to_response('ip.html', {'ip_info': result}, RequestContext(request))
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        result = resolve_country_from_ip(ip)
+        return render_to_response('ip.html', {'ip_info': result}, RequestContext(request))
+
+
+class LoginView(View):
+    def post(self, request, username, password):
+        pass
+
+
+class IPServiceView(View):
+    def get(self, request, ip_address):
+        pass
